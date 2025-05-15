719e87c9c2eb6c87a7cb4a0248063c2ffece7a3e
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-05-15 09:44:47 +0200

    fix: revised the new class

--- src/main/java/com/isabella/lektion2/PullRequest_Showcase.java
+++ src/main/java/com/isabella/lektion2/PullRequest_Showcase.java
@@ -2,4 +2,8 @@ package com.isabella.lektion2;
 
 public class PullRequest_Showcase {
     // REALLY BAD CODE
+
+    void newImplementation() {
+        System.out.println("New implementation");
+    }
 }
